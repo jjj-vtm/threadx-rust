ab55ebec2c718bac8e3bfca4efe80f59402a4062
Jan Jongen <jan.jongen@valtech.com>
2025-01-17 08:47:10 +0100

    Make allocator API more safe

--- threadx-app/cross/app/src/bin/event_flag.rs
+++ threadx-app/cross/app/src/bin/event_flag.rs
@@ -4,7 +4,6 @@
 use core::ffi::CStr;
 use core::sync::atomic::{AtomicBool, AtomicU32};
 
-
 use alloc::boxed::Box;
 use board::{BoardMxAz3166, LowLevelInit};
 
@@ -20,6 +19,9 @@ use threadx_rs::thread::Thread;
 use threadx_rs::tx_str;
 
 extern crate alloc;
+
+#[global_allocator]
+static GLOBAL: ThreadXAllocator = ThreadXAllocator::new();
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -52,9 +54,9 @@ fn main() -> ! {
 
             let (global_alloc_mem, next) = next.split_at_mut(1024);
             //let  heap_bytepool : BytePoolHandle = unsafe{BP1.initialize(tx_str!("pool2"), bp1_mem).unwrap()};
-            #[global_allocator]
-            static mut GLOBAL: ThreadXAllocator = ThreadXAllocator::new();
-            unsafe { GLOBAL.initialize(global_alloc_mem).unwrap() };
+
+            // Only after this call the global allocator is working, using Heap alloocations before should panic.
+            GLOBAL.initialize(global_alloc_mem).unwrap();
 
             // create events flag group
             static EVENT_GROUP: StaticCell<EventFlagsGroup> = StaticCell::new();
@@ -68,7 +70,7 @@ fn main() -> ! {
             static TIMER: StaticCell<Timer> = StaticCell::new();
             let timer = TIMER.init(Timer::new());
 
-            let timer_fn = Box::new( move || {
+            let timer_fn = Box::new(move || {
                 evt_handle.publish(1).unwrap();
             });
 
@@ -81,9 +83,9 @@ fn main() -> ! {
                     true,                               // start the timer immediately
                 )
                 .expect("Timer Init failed");
-            
+
             static mut thread: Thread = Thread::new();
-            
+
             let thread_func = Box::new(move || {
                 let mut arg: u32 = 0;
 
@@ -104,16 +106,22 @@ fn main() -> ! {
 
             let _ = unsafe {
                 thread
-                    .initialize_with_autostart_box("thread1", thread_func, task1_mem.consume(), 1, 1, 0)
+                    .initialize_with_autostart_box(
+                        "thread1",
+                        thread_func,
+                        task1_mem.consume(),
+                        1,
+                        1,
+                        0,
+                    )
                     .unwrap()
             };
-            
+
             let thread2_fn = Box::new(move || {
                 let arg: u32 = 1;
                 println!("Thread:{}", arg);
 
                 loop {
-
                     let event = evt_handle
                         .get(
                             1,
@@ -123,14 +131,20 @@ fn main() -> ! {
                         .expect("Thread2 failed");
                     println!("Thread2: Got Event 1 : {}", event);
                     threadx_rs::thread::sleep(core::time::Duration::from_millis(100)).unwrap();
-
                 }
             });
             static mut thread2: Thread = Thread::new();
 
             let th2_handle = unsafe {
                 thread2
-                    .initialize_with_autostart_box("thread1", thread2_fn, task2_mem.consume(), 1, 1, 0)
+                    .initialize_with_autostart_box(
+                        "thread1",
+                        thread2_fn,
+                        task2_mem.consume(),
+                        1,
+                        1,
+                        0,
+                    )
                     .unwrap()
             };
 
@@ -156,7 +170,14 @@ fn main() -> ! {
 
             let th3_handle = unsafe {
                 thread3
-                    .initialize_with_autostart_box("thread2", thread3_fn, task3_mem.consume(), 1, 1, 0)
+                    .initialize_with_autostart_box(
+                        "thread2",
+                        thread3_fn,
+                        task3_mem.consume(),
+                        1,
+                        1,
+                        0,
+                    )
                     .unwrap()
             };
 
--- threadx-rs/src/allocator.rs
+++ threadx-rs/src/allocator.rs
@@ -1,58 +1,76 @@
-use core::{alloc::{GlobalAlloc, Layout}, ffi::{c_void, CStr}, mem::MaybeUninit};
-use crate::{tx_checked_call, tx_str};
-use threadx_sys::{_tx_byte_allocate, TX_WAIT_FOREVER, ULONG, _tx_byte_release, TX_BYTE_POOL, _tx_byte_pool_create};
-use defmt::{error, println};
 use crate::error::TxError;
+use crate::{pool, tx_checked_call, tx_str};
+use core::cell::{Cell, RefCell, UnsafeCell};
+use core::sync::atomic::AtomicBool;
+use core::{
+    alloc::{GlobalAlloc, Layout},
+    ffi::{c_void, CStr},
+    mem::MaybeUninit,
+    ptr::null_mut,
+};
+use defmt::{error, println};
 use num_traits::FromPrimitive;
+use threadx_sys::{
+    _tx_byte_allocate, _tx_byte_pool_create, _tx_byte_release, TX_BYTE_POOL, TX_WAIT_FOREVER, ULONG,
+};
 
 /// ThreadX allocator for Rust. Instantiate this struct and use it as the global allocator.
-/// 
+///
 ///  `
 ///  #[global_allocator]
 ///  static mut GLOBAL: ThreadXAllocator = ThreadXAllocator::new();
 ///  unsafe{GLOBAL.initialize(bp1_mem).unwrap()};
 ///  `
-pub struct ThreadXAllocator(MaybeUninit<TX_BYTE_POOL>);
+
+// We use a static mut and initialize it to zero. After this we only work with raw pointers to this static mut to avoid UB by accidentally creating aliasing mut references
+static mut POOL_STRUCT: TX_BYTE_POOL = unsafe {
+    MaybeUninit::zeroed().assume_init()
+};
+
+pub struct ThreadXAllocator {
+    pool_ptr: *mut TX_BYTE_POOL,
+    initialized: AtomicBool,
+}
+
 unsafe impl Sync for ThreadXAllocator {}
 
 impl ThreadXAllocator {
     pub const fn new() -> Self {
-        ThreadXAllocator(MaybeUninit::<TX_BYTE_POOL>::uninit())
+        ThreadXAllocator {
+            pool_ptr: &raw mut POOL_STRUCT,
+            initialized: AtomicBool::new(false),
+        }
     }
 
-    pub fn initialize(
-        &'static mut self,
-        pool_memory: &mut [u8],
-    ) -> Result<(), TxError> {
-
-        let pool_ptr = self.0.as_mut_ptr();
-        if pool_ptr.is_null() {
-            panic!("Pool ptr is null");
-        }
-        unsafe {
-            if !(*pool_ptr).tx_byte_pool_start.is_null() {
-                panic!("Pool is already initialized");
-            }
-        }
-        tx_checked_call!(_tx_byte_pool_create(
-            pool_ptr,
+    pub fn initialize(&'static self, pool_memory: &mut [u8]) -> Result<(), TxError> {
+        // Panic if initialized twice. Check if name is not global (and not zero)
+        let res = tx_checked_call!(_tx_byte_pool_create(
+            self.pool_ptr,
             tx_str!("global").as_ptr() as *mut i8,
             pool_memory.as_mut_ptr() as *mut core::ffi::c_void,
             pool_memory.len() as ULONG
-        ))
+        ));
+        // Set the allocator to initialized
+        self.initialized.fetch_or(true, core::sync::atomic::Ordering::Relaxed);
+        res
     }
 }
 
 unsafe impl GlobalAlloc for ThreadXAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        if !self.initialized.load(core::sync::atomic::Ordering::Relaxed) {
+            panic!("Use of ThreadX allocator before it was initialized");
+        }
+        // TODO: Handle alignment
         let mut ptr: *mut c_void = core::ptr::null_mut() as *mut c_void;
         tx_checked_call!(_tx_byte_allocate(
-            self.0.as_ptr() as *mut _, 
+            self.pool_ptr,
             &mut ptr,
             layout.size() as ULONG,
             TX_WAIT_FOREVER
         ))
-        .map(|_| ptr as *mut u8 ).unwrap()
+        .map(|_| ptr as *mut u8)
+        .unwrap()
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
